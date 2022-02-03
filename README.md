3f96d2d1ed1d51f61367945222029033e6f0f8bc
name <name@gmail.com>
2022-02-03 23:52:04 +0800

    test github action

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# wecaht-windows-versions
-保存微信历史版本
+# wechat-windows-versions
+收集 Windows 微信版本并保存
+
+## 目录结构
+```shell
+├── README.md # 自述文件
+├── WeChatSetup # 微信安装包临时目录
+│   └── temp # 临时目录
+└── scripts   # 脚本目录
+    └── destVersionRelease.sh # 获取安装包及取得版本号与 hash 值的脚本
+```
+
+## 说明
+项目使用 Github Action 自动下载微信最新版本安装包计算 Hash 值并推送至仓库。
+
+**注意： 3.5.0.46 版本以下（不包含 3.5.0.46 版， 仅下载了一部分）均下载自 [web.archive.org](https://web.archive.org/web/*/https://pc.weixin.qq.com/)**
+
+各版本更新日志可参见 [changelog](https://weixin.qq.com/cgi-bin/readtemplate?lang=zh_CN&t=weixin_faq_list&head=true)
---
+++ scripts/destVersionRelease.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+temp_path="WeChatSetup/temp"
+latest_path="WeChatSetup/latest"
+
+download_link="$1"
+if [ -z "$1" ]; then
+    >&2 echo -e "Missing argument. Using default download link"
+    download_link="https://dldir1.qq.com/weixin/Windows/WeChatSetup.exe"
+fi
+
+function install_depends () {
+    printf "#%.0s" {1..60}
+    echo 
+    echo -e "## \033[1;33mInstalling 7zip, shasum, wget, curl, git\033[0m"
+    printf "#%.0s" {1..60}
+    echo 
+    apt install -y p7zip-full p7zip-rar libdigest-sha-perl wget curl git
+}
+
+function download_wechat() {
+    printf "#%.0s" {1..60}
+    echo 
+    echo -e "## \033[1;33mDownloading the newest WechatSetup...\033[0m"
+    printf "#%.0s" {1..60}
+    echo 
+    wget "$download_link" -O ${temp_path}/WeChatSetup.exe
+    if [ "$?" -ne 0 ]; then
+        >&2 echo -e "\033[1;31mDownload Failed, please check your network!\033[0m"
+        clean_data 1
+    fi
+}
+
+function extract_version() {
+    printf "#%.0s" {1..60}
+    echo 
+    echo -e "## \033[1;33mExtract WechatSetup, get the dest version of wechat\033[0m"
+    printf "#%.0s" {1..60}
+    echo 
+    
+    local outfile=`7z l ${temp_path}/WeChatSetup.exe | grep improve.xml | awk 'NR ==1 { print $NF }'`
+    ## 7z x ${temp_path}/WeChatSetup.exe -o${temp_path}/temp "\$R5/Tencent/WeChat/improve.xml"
+    7z x ${temp_path}/WeChatSetup.exe -o${temp_path}/temp $outfile
+    dest_version=`awk '/MinVersion/{ print $2 }' ${temp_path}/temp/$outfile | sed -e 's/^.*="//g' -e 's/".*$//g'`
+}
+
+
+# rename and replace
+function prepare_commit() {
+    printf "#%.0s" {1..60}
+    echo 
+    echo -e "## \033[1;33mPrepare to commit new version\033[0m"
+    printf "#%.0s" {1..60}
+    echo 
+    mkdir -p WeChatSetup/$dest_version
+    cp $temp_path/WeChatSetup.exe WeChatSetup/$dest_version/WeChatSetup-$dest_version.exe
+    echo "DestVersion: $dest_version" > WeChatSetup/$dest_version/WeChatSetup-$dest_version.exe.sha256
+    echo "Sha256: $now_sum256" >> WeChatSetup/$dest_version/WeChatSetup-$dest_version.exe.sha256
+    echo "UpdateTime: $(date -u '+%Y-%m-%d %H:%M:%S') (UTC)" >> WeChatSetup/$dest_version/WeChatSetup-$dest_version.exe.sha256
+    echo "DownloadFrom: $download_link" >> WeChatSetup/$dest_version/WeChatSetup-$dest_version.exe.sha256
+    
+}
+
+function clean_data() {
+    printf "#%.0s" {1..60}
+    echo 
+    echo -e "## \033[1;33mClean runtime and exit...\033[0m"
+    printf "#%.0s" {1..60}
+    echo 
+    rm -rfv WeChatSetup/*
+    exit $1
+}
+
+function main() {
+    rm -rfv WeChatSetup/*
+    mkdir -p ${temp_path}/temp
+    ## https://github.com/actions/virtual-environments/blob/main/images/linux/Ubuntu2004-Readme.md
+    # install_depends
+    download_wechat
+
+    now_sum256=`shasum -a 256 ${temp_path}/WeChatSetup.exe | awk '{print $1}'`
+    local latest_sum256=`gh release view  --json body --jq ".body" | awk '/Sha256/{ print $2 }'`
+
+    if [ "$now_sum256" = "$latest_sum256" ]; then
+        >&2 echo -e "\n\033[1;32mThis is the newest Version!\033[0m\n"
+        clean_data 0
+    fi
+    ## if not the newest
+    extract_version
+    prepare_commit
+
+    gh release create v$dest_version ./WeChatSetup/$dest_version/WeChatSetup-$dest_version.exe -F ./WeChatSetup/$dest_version/WeChatSetup-$dest_version.exe.sha256 -t "Wechat v$dest_version"
+    clean_data 0
+}
+
+main
+
